bb6a319f852b50ff9fc3f5c8ac385afb998bf372
Anton S <av.solodovnyk@gmail.com>
2019-10-21 01:41:17 +0300

    Updates

---
+++ precache-manifest.3d4d16ad647c80c5d62eee3579bc6ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf79ef169771c97effeab08c809b8a8e",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "fb7c4961ec1b6ddc54ca",
+    "url": "/goit-react-hw-02-reader/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7aac82e3655b45dacb0c",
+    "url": "/goit-react-hw-02-reader/static/css/main.10196bb1.chunk.css"
+  },
+  {
+    "revision": "fb7c4961ec1b6ddc54ca",
+    "url": "/goit-react-hw-02-reader/static/js/2.8e7b4972.chunk.js"
+  },
+  {
+    "revision": "7aac82e3655b45dacb0c",
+    "url": "/goit-react-hw-02-reader/static/js/main.620f4f2f.chunk.js"
+  },
+  {
+    "revision": "0da2320c246f0dcbed6e",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.21ebc90b.js"
+  }
+]);
